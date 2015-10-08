11caf98f88b225f0d3e59e9861c00c92985435a4
jcMedinaMtz <jcmedinamartinez@gmail.com>
2015-10-07 21:11:21 -0500

    Revert "Merge pull request #1 from unboundit/develop"
    This reverts commit b2b95025252a8f7884ad626ff5363a9442c3d487, reversing
    changes made to 9a1ac75293fc3ee9cfbc78db8ea28c0879ebde8a.

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!empty($_SESSION))
-{
-	session_destroy();
-}
-header("Location:index.php");
-
-?>
--- server/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'unboundi_abel');
-define('DB_PASSWORD', 'ubit_abel');
-define('DB_DATABASE', 'unboundi_reservax');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
